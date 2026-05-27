3dfb06975b7a06161c7dceef3bc4c861426dfe77
himath-adithya <himathadithya@gmail.com>
2026-05-27 21:15:25 +0530

    Removed functions.rs containing common funcs

--- src/functions.rs
+++
@@ -1,9 +0,0 @@
-// load memory into the CPU's memory
-pub fn load_memory(arg: &String) -> Vec<u8> {
-    let mem = std::fs::read(arg).expect("Failed to read the binary file");
-    // NOTE: this line below is not of real logic, therefore can be removed
-    if mem.len() % 4 != 0 {
-        panic!("Memory size is not a multiple of 4");
-    }
-    mem
-}
