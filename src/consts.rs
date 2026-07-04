85ce5e40950b18bf1e9d9f51347f6b384a85f6e7
himath-adithya <himathadithya@gmail.com>
2026-07-05 02:11:14 +0530

    Corrected constant names and values. REG_SIZE -> GPR_COUNT

--- src/consts.rs
+++ src/consts.rs
@@ -1,5 +1,5 @@
-pub const INST_SIZE: usize = 4;
-pub const WORD_SIZE: usize = 4;
+pub const INST_SIZE:     usize = 4;
+pub const WORD_SIZE:     usize = 4;
 pub const HALFWORD_SIZE: usize = 2;
-pub const BYTE_SIZE: usize = 1;
-pub const REG_SIZE: usize = 4;
+pub const BYTE_SIZE:     usize = 1;
+pub const GPR_COUNT:     usize = 32;
